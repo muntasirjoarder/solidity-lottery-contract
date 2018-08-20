98970aaf205690226009b5acb90ae6d1694a51c6
Muntasir Joarder <muntasir.joarder@gmail.com>
2018-08-20 12:36:52 +1000

    Second commit

---
+++ deploy.js
@@ -0,0 +1,20 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
+
+const provider = new HDWalletProvider(
+    'cute moral pluck ring truly eternal dice reduce lizard galaxy consider brother',
+    'https://rinkeby.infura.io/eb1a83450c1d403292a48b4b1e65a8f1'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account ', accounts[0]);
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: '0x'+bytecode})
+        .send({from: accounts[0]});
+};
+
+deploy();
